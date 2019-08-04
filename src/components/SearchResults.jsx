3aed5acd1380ea23fedfd05b4b09177d97d25d39
Rob Conway <robconway12@gmail.com>
2019-08-04 13:41:59 -0700

    added welcome and serach results page

---
+++ src/components/SearchResults.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import '../App.css';
+
+const SearchResults = props => (
+  <div id="results-container">
+      Search Results
+  </div>
+);
+
+export default SearchResults;
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import '../App.css';
+
+const Welcome = props => (
+  <div id="welcome-container">
+      Welcome
+  </div>
+);
+
+export default Welcome;
