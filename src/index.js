97ff28a01285a23106a44990a723e1ec68b19b70
Rob Conway <robconway12@gmail.com>
2019-08-04 14:41:15 -0700

    added routing

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
-import App from './App'
-import Welcome from './welcome'
-import SearchResults from './searchResults'
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from 'react-dom'
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+import App from "./App";
+import Welcome from "./welcome";
+import SearchResults from "./searchResults";
+import * as serviceWorker from "./serviceWorker";
 
 const routing = (
-    <Router>
-      <div>
-        <Route path="/" component={Welcome} />
-        <Route path="/app" component={App} />
-        <Route path="/searchresults" component={SearchResults} />
-      </div>
-    </Router>
-  )
+  <Router>
+    <div>
+      <Route exact path="/" component={Welcome} />
+      <Route path="/app" component={App} />
+      <Route path="/searchresults" component={SearchResults} />
+    </div>
+  </Router>
+);
 
-ReactDOM.render(routing, document.getElementById('root'));
+ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
