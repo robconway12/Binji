b6bfd0b0563029034c8de0a64ad679e70d3ceac0
Rob Conway <robconway12@gmail.com>
2019-08-03 23:12:55 -0700

    worked on CSS

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import '../App.css';
 
 const Footer = props => (
-  <div className="container" id="footer-container">
-      <p id="footer-copyright">&#9400; Copyright 2019  Binji</p>
+  <div className="container fixed-bottom" id="footer-container">
+      <p id="footer-copyright" className="float-left img-fluid">&#9400; Copyright 2019  Binji</p>
+      <img src={require('../images/TMDB-logo.png')} style={{ maxWidth: 120 }} className="float-right mb-2" alt="TMDB Logo" id="tmdb-logo"></img>
   </div>
 );
 
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,52 +1,53 @@
 import React from "react";
-import '../App.css';
+import "../App.css";
 
 import { program } from "@babel/types";
 import SearchForm from "./SearchForm";
 
-function MainPage() {  
-
-  return(
+function MainPage() {
+  return (
     <>
-        <br />
-        <SearchForm />
+      <br />
+      <SearchForm />
 
-        <table className="table">
-            <thead>
-                <tr>
-                    <th>TMDB ID</th>
-                    <th>Title</th>
-                     {/* <th>Genre</th>  */}
-                    <th>Release Date</th>
-                    <th>Cover Art</th>
-                    <th>Summary</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                {/* React code to map through database info */}
-                    <tr>
-                        <td >{program.progID}</td>
-                        <td>{program.title}</td>
-                        {/* <td>{program.genre}</td>  */}
+      <table className="table mx-auto">
+        <thead>
+          <tr>
+            <th>TMDB ID</th>
+            <th>Title</th>
+            {/* <th>Genre</th>  */}
+            <th>Release Date</th>
+            <th>Cover Art</th>
+            <th>Summary</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          {/* React code to map through database info */}
+          <tr>
+            <td>{program.progID}</td>
+            <td>{program.title}</td>
+            {/* <td>{program.genre}</td>  */}
 
-                        <td>{program.year}</td>
-                        <td>
-                            <img src={program.imgURL} alt=""/>
-                        </td>
-                        <td>{program.overview}</td>
-                        <td >
-                            
-                            <a href="/title/delete/:" id={program.progID} className ="btn btn-danger deleteBtn">Delete</a>
-                        </td>
-                        
-                    </tr>
-
-            </tbody>
-        </table>
-
-</> 
-)}
-
+            <td>{program.year}</td>
+            <td>
+              <img src={program.imgURL} alt="" />
+            </td>
+            <td>{program.overview}</td>
+            <td>
+              <a
+                href="/title/delete/:"
+                id={program.progID}
+                className="btn btn-danger deleteBtn"
+              >
+                Delete
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+}
 
 export default MainPage;
