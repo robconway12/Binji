e0fa1cd5776efb0a4811c9949be557b06560820e
Rob Conway <robconway12@gmail.com>
2019-08-03 21:04:28 -0700

    added navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import '../App.css';
 
 const Header = props => (
   <div className="container" id="header-container">
-      "Header"
+    
   </div>
 );
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,9 +2,25 @@ import React from "react";
 import '../App.css';
 
 const NavBar = props => (
-    <nav className="container" id="navBar-container">
-        "NavBar"
-    </nav>
+<nav class="navbar navbar-expand-lg navbar-light bg-light" id="nav">
+  <a class="navbar-brand" href="#" id="nav-BinjiLogo">Binji</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#" id="nav-tv">TV<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" id="nav-movies">Movies</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" id="nav-podcasts">Podcasts</a>
+      </li>
+    </ul>
+  </div>
+</nav>
 )
 
 export default NavBar;
