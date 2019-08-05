e647e1ff78935da80398e7fc08cdbeedbdf8607e
Carolyn <carolynwk@gmail.com>
2019-08-04 20:34:53 -0700

    minor changes

--- src/components/WatchList.jsx
+++ src/components/WatchList.jsx
@@ -8,25 +8,24 @@ class WatchList extends React.Component {
     results: []
   };
   // When this component mounts, query the db for watchlist
-  componentDidMount() {
-    var express = require("express");
-    var router = express.Router();
+  // componentDidMount() {
+  //   var express = require("express");
+  //   var router = express.Router();
 
-    /* GET users listing. */
-    router.get("/", function(req, res, next) {
-      res.locals.connection.query("SELECT * from members", function(
-        error,
-        results,
-        fields
-      ) {
-        if (error) throw error;
-        res.send(JSON.stringify(results));
-      });
-    });
-console.log("Component did mount");
+  //   console.log("Component did mount");
+  //   /* GET users listing. */
+  //   router.get("/", function(req, res, next) {
+  //     res.locals.connection.query("SELECT * from members", function(
+  //       error,
+  //       results,
+  //       fields
+  //     ) {
+  //       if (error) throw error;
+  //       res.send(JSON.stringify(results));
+  //     });
+  //   });
 
-    module.exports = router;
-  }
+  // }
 
   render() {
     var props = "";
