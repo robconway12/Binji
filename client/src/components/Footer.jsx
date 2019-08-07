2da01052a3df7ac2f9a03e36a8606c59e47acedd
Luba <sapufira@gmail.com>
2019-08-06 20:59:24 -0700

    footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import '../App.css';
 
 const Footer = props => (
-  <div className="container fixed-bottom" id="footer-container">
+  <div className="container" id="footer-container">
       <p id="footer-copyright" className="float-left img-fluid">&#9400; Copyright 2019  Binji</p>
       <img src={require('../images/TMDB-logo.png')} style={{ maxWidth: 120 }} className="float-right mb-2" alt="TMDB Logo" id="tmdb-logo"></img>
   </div>
